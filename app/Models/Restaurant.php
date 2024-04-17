1799b4de440825ffb1364811b3bde01138fe7ba8
ariannacostanzo <costanzo.arianna@outlook.it>
2024-04-17 13:23:35 +0200

    handlign relation between restaurants and dishes

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -13,4 +13,9 @@ class Restaurant extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function dishes()
+    {
+        return $this->hasMany(Dish::class);
+    }
 }
---
+++ database/migrations/2024_04_17_105355_create_dishes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dishes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('ingredients');
+            $table->decimal('price');
+            $table->boolean('is_visible')->default(1);
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dishes');
+    }
+};
