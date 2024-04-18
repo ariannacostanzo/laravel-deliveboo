6e582b1643ba1a55733e01893276f1309988b6ea
Mateusz Kacperski <kacperskimateusz95@gmail.com>
2024-04-18 17:38:19 +0200

    create type seeder

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        $types = [
+            [
+                'label' => 'Italiano',
+                'icon' => 'https://img.freepik.com/premium-vector/italian-food-logo-name-icon-symbol-vector-italy_526280-402.jpg',
+            ],
+            [
+                'label' => 'Sushi',
+                'icon' => 'https://img.freepik.com/premium-vector/sushi-with-chopstick-cartoon-icon-illustration-japanese-food-icon-concept-isolated-flat-cartoon-style_138676-1723.jpg',
+            ],
+            [
+                'label' => 'Cinese',
+                'icon' => 'https://st3.depositphotos.com/8950810/31734/v/950/depositphotos_317342607-stock-illustration-cute-ramen-bowl-isolated-on.jpg',
+            ],
+            [
+                'label' => 'Pizza',
+                'icon' => 'https://static.vecteezy.com/ti/vettori-gratis/p3/26783111-carino-pizza-logo-portafortuna-vettore-icona-illustrazione-cibo-cartone-animato-piatto-stile-adatto-per-ristorante-vettoriale.jpg',
+            ],
+            [
+                'label' => 'Kebab',
+                'icon' => 'https://static.vecteezy.com/ti/vettori-gratis/p3/21523821-illustrazione-vettore-grafico-di-kebab-duro-falafel-turco-cibo-cibo-avvolgere-icona-gratuito-vettoriale.jpg',
+            ],
+            [
+                'label' => 'Griglieria',
+                'icon' => 'https://macelleriabelli.it/wp-content/uploads/2020/07/Belli_shop1926.jpg',
+            ],
+        ];
+
+
+        foreach($types as $type){
+            $new_type = new Type();
+            $new_type->label = $type['label'];
+            $new_type->icon = $type['icon'];
+        
+            $new_type->save();
+        }
+    }
+}
