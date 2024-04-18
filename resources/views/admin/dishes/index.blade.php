260beafce629cedff9baa0e4002603a99fa7ed9f
g1ammi93 <giammariacarelli93@hotmail.it>
2024-04-18 17:44:03 +0100

    Added Search Bar

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -7,6 +7,20 @@
     <header>
         <h1 class="text-center my-4">Piatti</h1>
     </header>
+
+    {{-- Filtro di ricerca per i piatti --}}
+    <div class="d-flex justify-content-end">
+        <form method="GET" action="{{ route('dishes.index') }}" class="w-25">
+            <div class="input-group mb-3">
+                <input type="search" class="form-control" placeholder="Cerca..." name="search" value="{{ $search }}"
+                    autofocus>
+                <div class="input-group-append">
+                    <button class="btn btn-outline-primary">Cerca</button>
+                </div>
+            </div>
+        </form>
+    </div>
+
     <table class="table table-striped container">
         <thead>
             <tr>
@@ -39,16 +53,20 @@
                         @endif
                     </td>
                     <td>{{ $dish->price }} €</td>
-                    <td> <img class="image-small" src="{{ $dish->image ? $dish->image : 'https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg' }}"  alt="{{ $dish->name }}" > </td>
+                    <td> <img class="image-small"
+                            src="{{ $dish->image ? $dish->image : 'https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg' }}"
+                            alt="{{ $dish->name }}"> </td>
                     <td>
-                        <form action="{{ route('admin.dishes.visible', $dish->id) }}" method="POST" class="visible-form" onclick="this.submit()">
+                        <form action="{{ route('admin.dishes.visible', $dish->id) }}" method="POST" class="visible-form"
+                            onclick="this.submit()">
                             @csrf
                             @method('PATCH')
                             <div class="form-check form-switch">
-                                <input class="form-check-input" type="checkbox" role="button" id="{{'is_visible' . $dish->id}}" name="is_visible"
+                                <input class="form-check-input" type="checkbox" role="button"
+                                    id="{{ 'is_visible' . $dish->id }}" name="is_visible"
                                     @if ($dish->is_visible) checked @endif>
                                 <label class="form-check-label"
-                                    for="{{'is_visible' . $dish->id}}">{{ $dish->is_visible ? 'Visibile' : 'Non Visibile' }}</label>
+                                    for="{{ 'is_visible' . $dish->id }}">{{ $dish->is_visible ? 'Visibile' : 'Non Visibile' }}</label>
                             </div>
                         </form>
                     </td>
@@ -62,9 +80,10 @@
                             <a href="{{ route('admin.dishes.edit', $dish) }}" class="btn btn-sm btn-warning"> <i
                                     class="fas fa-pencil"></i></a>
                             <form action="{{ route('admin.dishes.destroy', $dish) }}" method="POST" class="delete-form">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-sm btn-danger"><i class="fas fa-trash-can"></i></button>
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-sm btn-danger"><i
+                                        class="fas fa-trash-can"></i></button>
                             </form>
                         </div>
                     </td>
