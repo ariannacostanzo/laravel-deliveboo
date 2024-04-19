c943ae16c2f1d896845dada750e4da3f3e67481e
Mateusz Kacperski <kacperskimateusz95@gmail.com>
2024-04-19 17:24:39 +0200

    wesdfsd

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,13 +8,12 @@
 
 <div class="container">
     <h2 class="fs-4 text-secondary my-4">
-        {{ __('Dashboard') }}
     </h2>
     <div class="row justify-content-center">
-        
+
         <div class="col">
             <div class="card">
-                <div class="card-header">Benvenuto </div>
+                <div class="card-header">Benvenuto {{$user->name}}, queste sono le informazioni del tuo ristorante:</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -22,13 +21,28 @@
                         {{ session('status') }}
                     </div>
                     @endif
-
-                    {{ __('You are logged in!') }}
+                    <div class="d-flex justify-content-between">
+                        <div class="d-flex flex-column justify-content-between">
+                            <h2>{{$restaurant->name}}</h2>
+                            <div>
+                                <p class="my-3"> <strong>Indirizzo:</strong> {{$restaurant->address}}</p>
+                                <p><strong>Partita IVA: </strong>{{$restaurant->vat_number}}</p>
+                            </div>
+                        </div>
+                        <div>
+                            <figure class="restaurant-image">
+                                <img src="{{$restaurant->image}}" alt="{{$restaurant->name}}">
+                            </figure>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+
+
 @else
 
 <h1 class="text-center">form registrazione ristorante</h1>
